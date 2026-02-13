be3d853389aeb652a28cdea9c47825ce9e1af5c6
Anandhu4444 <hey4444567889987@gmail.com>
2026-02-13 10:15:52 +0530

    StudentClass.java

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -1,45 +1,43 @@
 import java.util.Scanner;
 
 class Student {
-   
-   String name;
-   int rollNo;
-    // TODO: Create a parameterized constructor Student(String name, int rollNo)
-   Student(String name, int rollNo){
-      this.name=name;
-      this.rollNo=rollNo;
-   }
-    
-    // TODO: Create a method or use direct access to print details in the required format
-    void display(){
+    String name;
+    int rollNo;
 
-      System.out.print("name:"+name+" rollno:"+rollNo);
-   }
+    // Parameterized Constructor
+    Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    // Method to display details
+    void display() {
+        System.out.println("Name: " + name + " Roll: " + rollNo);
+    }
 }
 
 public class StudentClass {
-   public static void main{ 
-        
-        // TODO: Read name1
-         Scanner sc=new Scanner(System.in);
-         String name1=sc.nextLine();
-         int rollNo1=sc.nextInt();
-         sc.nextLine();
-        // TODO: Read rollNo1
-        
-        // Hint: Handle the newline consumption if using nextLine() after nextInt()
-        
-        // TODO: Read name2
-         String name2=sc.nextLine();
-         int rollNo2=sc.nextInt();
-        // TODO: Read rollNo2
-        
-        // TODO: Create two Student objects
-         Student s1=new Student(name1,rollNo1);
-         Student s2=new Student(name2,rollNo2);
-        
-        // TODO: Print details for both students
-         s1.display();
-         s2.display();
-   }  
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Read student 1 details
+        String name1 = sc.nextLine();
+        int rollNo1 = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        // Read student 2 details
+        String name2 = sc.nextLine();
+        int rollNo2 = sc.nextInt();
+
+        // Create objects
+        Student s1 = new Student(name1, rollNo1);
+        Student s2 = new Student(name2, rollNo2);
+
+        // Print details
+        s1.display();
+        s2.display();
+
+        sc.close();
+    }
 }
